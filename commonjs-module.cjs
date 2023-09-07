8147468bf7a70b4805cd910b801bc3e77efee874
pedromoussa <pedropaulo.moussa@gmail.com>
2023-09-07 19:53:34 -0300

    fix path.join to build path correctly

--- commonjs-module.cjs
+++ commonjs-module.cjs
@@ -3,7 +3,7 @@ module.exports.program = program;
 
 const path = require('path');
 module.exports.createEnvironment = async function (templatePath) {
-  const absolutePath = `file://${path.join(__dirname, templatePath)}`;
+  const absolutePath = `file://${path.join(__dirname, templatePath).replace(/\\/g, '/')}`;
   const createEnvironment = await import(absolutePath);
   createEnvironment();
 };
